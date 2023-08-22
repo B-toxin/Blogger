ba6bf1e95ed79798518e4365c9b2cb30781d9cac
B-toxin <142661535+B-toxin@users.noreply.github.com>
2023-08-22 23:26:45 +0100

    Add files via upload

---
+++ FlaskBlog/config.py
@@ -0,0 +1,9 @@
+class Config:
+    SECRET_KEY = 'e71121f8359c7c241f56e489f91f32d7'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://dblog_5d0l_user:jEwPqP04nWhUTPh54eiIBNGTVuojUgY5@dpg-cjg8snb37aks73b73np0-a.ohio-postgres.render.com/dblog_5d0l'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'caasi9092@gmail.com'
+    MAIL_PASSWORD = 'wrxcyfiacptxmlib'
+    STATIC_FOLDER = 'static'
